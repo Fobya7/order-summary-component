d442aa27939cca8ec0906aeb59597d8634bcd6cb
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-08-31 15:44:47 +0200

    path tweaks

--- src/app/order-summary/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary/order-summary.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'order-summary',
-    templateUrl: './order-summary.component.html',
+    templateUrl: 'order-summary.component.html',
     styleUrls: [
-        './scss/colors.scss',
-        './scss/typography.scss',
-        './scss/layout.scss'
+        'scss/colors.scss',
+        'scss/typography.scss',
+        'scss/layout.scss'
         ]
     })
 export class OrderSummaryComponent implements OnInit
